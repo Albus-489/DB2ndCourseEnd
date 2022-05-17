cdc4952a03e31b14163f317ce4900dd672f8b508
bavrz <bavrz@gmail.com>
2022-05-17 13:09:37 +0300

    p

--- Lab8db/Frontend/src/app/app.component.ts
+++ Lab8db/Frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ShowComponent } from './show/show.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  showTable(){
+    location.href = '/show'
+  }
 }
