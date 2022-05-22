89bb7a012a76d6c85dc300fcbe24366cc70eb34a
bavrz <bavrz@gmail.com>
2022-05-22 18:48:11 +0300

    Toolbar nav update

--- Lab10db/Frontend/Lab10/src/app/app-routing.module.ts
+++ Lab10db/Frontend/Lab10/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ClientsComponent } from './clients/clients.component';
+import { InvestComponent } from './invest/invest.component';
+import { SecurComponent } from './secur/secur.component';
 
 const routes: Routes = [
-  //{path: '', component: AppComponent},
-  {path: 'Clients', component: ClientsComponent}
+  // {path: '', component: AppComponent},
+  {path: 'Clients', component: ClientsComponent},
+  {path: 'Secur', component: SecurComponent},
+  {path: 'Invest', component: InvestComponent}
 ];
 
 @NgModule({
